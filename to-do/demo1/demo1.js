658e889043b71a0fea3b34e44c223129f56c1cb5
PadNim14 <npadmana@purdue.edu>
2021-09-18 16:22:34 -0400

    testing

---
+++ to-do/demo1/demo1.js
@@ -0,0 +1,14 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: "I changed it.",
+        other_message: "Variables.",
+    }
+});
+
+const newapp = new Vue({
+    el: '#newapp',
+    data: {
+        mess1: "Here is more text"
+    }
+});
